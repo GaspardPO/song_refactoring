00dfa3579183a3813c1304d0ef7b668bf65dccef
GaspardPO <gaspard.pointeau@gmail.com>
2018-04-03 18:05:17 +0200

    refactor singular / plural for bottles

--- src/beer.rb
+++ src/beer.rb
@@ -1,20 +1,17 @@
 class BeerSong
   def lyrics
     returned_lyrics = ""
-    (1..97).each do |verseNumber|
+    (1..98).each do |verseNumber|
       number_of_bottles = 100 - verseNumber
       returned_lyrics <<
-          "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
-Take one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall.
+          "%s of beer on the wall, %s of beer.
+Take one down and pass it around, %s of beer on the wall.
 
-"
+" % [bottle(number_of_bottles), bottle(number_of_bottles), bottle(number_of_bottles - 1)]
     end
 
     returned_lyrics <<
-        "2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.
-
-1 bottle of beer on the wall, 1 bottle of beer.
+        "1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
 
 No more bottles of beer on the wall, no more bottles of beer.
@@ -22,5 +19,9 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
 "
     returned_lyrics
 
+  end
+
+  def bottle(number_of_bottles)
+    "#{number_of_bottles} bottle" +  (number_of_bottles <= 1 ? "" : "s")
   end
 end
